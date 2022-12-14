be722b4acc39e0c9fef8e88b90e6f37e4e0c8c00
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2022-12-14 10:13:21 -0700

    Update run.ps1

--- CISstandard/run.ps1
+++ CISstandard/run.ps1
@@ -6,6 +6,10 @@ param($Request, $TriggerMetadata)
 $APIName = $TriggerMetadata.FunctionName
 Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
 
+#Tokens for later
+$bodypasswordresetpol = "resource=74658136-14ec-4630-ad9b-26e160ff0fc6&grant_type=refresh_token&refresh_token=$($ENV:ExchangeRefreshToken)"
+$tokensspr = Invoke-RestMethod $uri -Body $bodypasswordresetpol -ContentType 'application/x-www-form-urlencoded' -ErrorAction SilentlyContinue -Method post
+
 
 # Write to the Azure Functions log stream.
 Write-Host 'PowerShell HTTP trigger function processed a request.'
@@ -22,7 +26,7 @@ if ($TenantFilter -eq 'AllTenants') {
 
 #Build Result Table
 $Result = @{
-    Tenant                           = "$($TenantName.displayName)"
+    Tenant                           = "$TenantFilter"
     ATPEnabled                       = ''
     HasAADP1                         = ''
     HasAADP2                         = ''
@@ -45,7 +49,7 @@ $Result = @{
     
 # Get the All results needed from the Secure Score
 try {
-    $SecureScore = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/security/secureScores?`$top=1" -tenantid $tenant -noPagination $true
+    $SecureScore = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/security/secureScores?`$top=1" -tenantid $Tenantfilter -noPagination $true
     $Result.ATPEnabled = $SecureScore.enabledServices.Contains("HasEOP")
     $Result.HasAADP1 = $SecureScore.enabledServices.Contains("HasAADP1")
     $Result.HasAADP2 = $SecureScore.enabledServices.Contains("HasAADP2")
@@ -60,13 +64,11 @@ try {
 
 }
 catch {
-    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $tenant -message "Secure Score Retrieval on $($tenant). Error: $($_.exception.message)" -sev 'Error' 
+    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $Tenantfilter -message "Secure Score Retrieval on $($Tenantfilter). Error: $($_.exception.message)" -sev 'Error' 
 }
 
 # Get Self Service Password Reset State
-try {
-    $bodypasswordresetpol = "resource=74658136-14ec-4630-ad9b-26e160ff0fc6&grant_type=refresh_token&refresh_token=$($ENV:ExchangeRefreshToken)"
-    $tokensspr = Invoke-RestMethod $uri -Body $bodypasswordresetpol -ContentType 'application/x-www-form-urlencoded' -ErrorAction SilentlyContinue -Method post
+try {    
     $SSPRGraph = Invoke-RestMethod -ContentType 'application/json;charset=UTF-8' -Uri 'https://main.iam.ad.ext.azure.com/api/PasswordReset/PasswordResetPolicies' -Method GET -Headers @{
         Authorization            = "Bearer $($tokensspr.access_token)";
         'x-ms-client-request-id' = [guid]::NewGuid().ToString();
@@ -78,8 +80,14 @@ try {
     If ($SSPRGraph.enablementType -eq 1) { $Result.SelfServicePasswordReset = 'Specific Users' }
     If ($SSPRGraph.enablementType -eq 2) { $Result.SelfServicePasswordReset = 'On' }
     If ([string]::IsNullOrEmpty($SSPRGraph.enablementType)) { $Result.SelfServicePasswordReset = 'Unknown' }
+}
+catch {
+    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $Tenantfilter -message "Self Service Password Reset on $($Tenantfilter). Error: $($_.exception.message)" -sev 'Error' 
+}
+
 
     # Check On Premise Password Protection
+try {
     $OPPPGraph = Invoke-RestMethod -ContentType 'application/json;charset=UTF-8' -Uri 'https://main.iam.ad.ext.azure.com/api/AuthenticationMethods/PasswordPolicy' -Method GET -Headers @{
         Authorization            = "Bearer $($tokensspr.access_token)";
         'x-ms-client-request-id' = [guid]::NewGuid().ToString();
@@ -90,36 +98,36 @@ try {
     $Result.enableBannedPassworCheckOnPremise = $OPPPGraph.enableBannedPassworCheckOnPremise
 }
 catch {
-    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $tenant -message "On Premise Password Protection on $($tenant). Error: $($_.exception.message)" -sev 'Error' 
+    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $Tenantfilter -message "On Premise Password Protection on $($Tenantfilter). Error: $($_.exception.message)" -sev 'Error' 
 }
 
 # Check JIT Access Packages
 try {
     
-    $JIT = New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/identityGovernance/entitlementManagement/accessPackages' -tenantid $tenant
+    $JIT = New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/identityGovernance/entitlementManagement/accessPackages' -tenantid $Tenantfilter
     $JITCount = $JIT | measure-object -Property id | select-object -ExpandProperty count
     $Result.accessPackages = if(!$JitCount){[int]"0"}else{$JitCount}
 }
 catch {
-    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $tenant -message "JIT Access Packages on $($tenant) Error: $($_.exception.message)" -sev 'Error'
+    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $Tenantfilter -message "JIT Access Packages on $($Tenantfilter) Error: $($_.exception.message)" -sev 'Error'
 }
 
 # Get the Secure Default State
 try {
-    $SecureDefaultsState = (New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/policies/identitySecurityDefaultsEnforcementPolicy' -tenantid $tenant)
+    $SecureDefaultsState = (New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/policies/identitySecurityDefaultsEnforcementPolicy' -tenantid $Tenantfilter)
     $Result.SecureDefaultState = $SecureDefaultsState.IsEnabled
 }
 catch {
-    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $tenant -message "Security Defaults State on $($tenant) Error: $($_.exception.message)" -sev 'Error'
+    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $Tenantfilter -message "Security Defaults State on $($Tenantfilter) Error: $($_.exception.message)" -sev 'Error'
 }
 
 # Admin Users Session CA Policy
 try {
-    $CAPolicies = New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/identity/conditionalAccess/policies' -tenantid $tenant
+    $CAPolicies = New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/identity/conditionalAccess/policies' -tenantid $Tenantfilter
     $Result.AdminSessionbyCA = ($CAPolicies | where {$_.conditions.users.includeRoles -ne $null -and $_.conditions.applications.includeApplications -eq "All" -and $_.sessionControls.persistentBrowser.mode -eq "never" -and $_.sessionControls.persistentBrowser.IsEnabled -eq "True"} | Select-object -ExpandProperty Displayname | measure-object).count
 }
 catch {
-    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $tenant -message "MFA Enforced by CA on $($tenant) Error: $($_.exception.message)" -sev 'Error'
+    Write-LogMessage -API 'CISstandardsAnalyser' -tenant $Tenantfilter -message "MFA Enforced by CA on $($Tenantfilter) Error: $($_.exception.message)" -sev 'Error'
 }
 
 #Display Results
