94212debfab78833952875fe14b0af08c76485e7
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2022-12-21 09:56:38 -0700

    Deploy ListPIM

---
+++ ListPIM/run.ps1
@@ -0,0 +1,38 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$APIName = $TriggerMetadata.FunctionName
+Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
+$Tenantfilter = $request.Query.tenantfilter
+
+try {
+    $eligbleschedules = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/roleManagement/directory/roleEligibilitySchedules?`$expand=roleDefinition,principal" -tenantid $Tenantfilter
+    $EligibleSchedulesSplat = @()
+foreach ($EligibleSchedule in $eligbleschedules){
+$object = [PSCustomObject]@{
+    principalId = $EligibleSchedule.principalID
+    roleDefinitionId = $EligibleSchedule.roleDefinitionId
+    roleDisplayName = $EligibleSchedule.roleDefinition.displayName
+    accountEnabled = $EligibleSchedule.principal.accountEnabled
+    displayName = $EligibleSchedule.principal.displayName
+    userPrincipalName = $EligibleSchedule.principal.userPrincipalName
+    status = $EligibleSchedule.status
+    startDateTime = $EligibleSchedule.scheduleInfo.startDateTime
+    expiration = $EligibleSchedule.scheduleInfo.expiration
+ }
+    $EligibleSchedulesSplat += $object
+    }
+}
+catch {
+    $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+    $StatusCode = [HttpStatusCode]::Forbidden
+    $EligibleSchedulesSplat = $ErrorMessage
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = $StatusCode
+        Body       = @($EligibleSchedulesSplat)
+    })
