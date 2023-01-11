a15490c2d445d599fa33e1b8620cd6d294d7d6c2
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-01-11 16:12:31 -0700

    Update run.ps1

--- NCListClients/run.ps1
+++ NCListClients/run.ps1
@@ -7,9 +7,44 @@ param($Request, $TriggerMetadata)
 Write-Host 'PowerShell HTTP trigger function processed a request.'
 
 # Interact with query parameters or the body of the request.
-New-NCentralConnection -ServerFQDN "$ENV:NCsite" -JWT "$ENV:NCJWTTOKEN"
 
-$CustomerList = Get-NCCustomerList | Where-Object parentid -eq "50" | select-object customername, customerid
+$MySoapRequest =(@"
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ei2="http://ei2.nobj.nable.com/">
+	<soap:Header/>
+	<soap:Body>
+		<ei2:customerListChildren>
+			<ei2:username></ei2:username>
+			<ei2:password>$ENV:NCJWTTOKEN</ei2:password>
+			<ei2:settings>
+				<ei2:key>customerID</ei2:key>
+				<ei2:value>50</ei2:value>
+			</ei2:settings>
+		</ei2:customerListChildren>
+	</soap:Body>
+</soap:Envelope>
+"@)
+
+## Set the Request-properties in a local Dictionary / Hash-table.
+$RequestProps = @{}
+$RequestProps.Method = "Post"
+$RequestProps.Uri = "$ENV:NCsite/dms2/services2/ServerEI2?wsdl HTTP/1.1"
+$RequestProps.TimeoutSec = 10
+$RequestProps.body =  $MySoapRequest
+
+$FullResponse = Invoke-RestMethod @RequestProps
+
+$Names = $FullResponse.Envelope.body.customerListChildrenResponse.return.items | where-object key -eq "customer.customername" | Select-Object -ExpandProperty value
+$IDs = $FullResponse.Envelope.body.customerListChildrenResponse.return.items | where-object key -eq "customer.customerid" | select-object -ExpandProperty value
+
+
+[int]$max = $Names.count
+$CustomerList = for ($i = 0; $i -lt $max; $i ++)
+{
+[pscustomobject]@{
+    customername = $Names[$i]
+    customerid = $IDs[$i]
+}
+}
 
 
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
