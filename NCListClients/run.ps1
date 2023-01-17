42675c5508edd0090b88d5f16d1e599b221bb261
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-01-17 12:46:41 -0700

    Update run.ps1

--- NCListClients/run.ps1
+++ NCListClients/run.ps1
@@ -9,9 +9,8 @@ Write-Host 'PowerShell HTTP trigger function processed a request.'
 # Interact with query parameters or the body of the request.
 $Table = Get-CIPPTable -TableName cacheNCclients
 $Rows = Get-AzDataTableEntity @Table | Where-Object -Property Timestamp -GT (Get-Date).AddHours(-8)
-if (!$Rows) {
 
-	$MySoapRequest = (@"
+$MySoapRequest = (@"
 <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ei2="http://ei2.nobj.nable.com/">
 	<soap:Header/>
 	<soap:Body>
@@ -27,33 +26,30 @@ if (!$Rows) {
 </soap:Envelope>
 "@)
 
-	## Set the Request-properties in a local Dictionary / Hash-table.
-	$RequestProps = @{}
-	$RequestProps.Method = "Post"
-	$RequestProps.Uri = "$ENV:NCsite/dms2/services2/ServerEI2?wsdl HTTP/1.1"
-	$RequestProps.TimeoutSec = 10
-	$RequestProps.body = $MySoapRequest
+## Set the Request-properties in a local Dictionary / Hash-table.
+$RequestProps = @{}
+$RequestProps.Method = "Post"
+$RequestProps.Uri = "$ENV:NCsite/dms2/services2/ServerEI2?wsdl HTTP/1.1"
+$RequestProps.TimeoutSec = 10
+$RequestProps.body = $MySoapRequest
 
-	$FullResponse = Invoke-RestMethod @RequestProps
+$FullResponse = Invoke-RestMethod @RequestProps
 
-	$Names = $FullResponse.Envelope.body.customerListChildrenResponse.return.items | where-object key -eq "customer.customername" | Select-Object -ExpandProperty value
-	$IDs = $FullResponse.Envelope.body.customerListChildrenResponse.return.items | where-object key -eq "customer.customerid" | select-object -ExpandProperty value
+$Names = $FullResponse.Envelope.body.customerListChildrenResponse.return.items | where-object key -eq "customer.customername" | Select-Object -ExpandProperty value
+$IDs = $FullResponse.Envelope.body.customerListChildrenResponse.return.items | where-object key -eq "customer.customerid" | select-object -ExpandProperty value
 
 
-	[int]$max = $Names.count
-	$CustomerList = for ($i = 0; $i -lt $max; $i ++) {
-		@{
-			customername = $Names[$i]
-			customerid   = $IDs[$i]
-			PartitionKey = "NCclients"
-			RowKey       = $IDs[$i]
-		}
+[int]$max = $Names.count
+$CustomerList = for ($i = 0; $i -lt $max; $i ++) {
+	@{
+		customername = $Names[$i]
+		customerid   = $IDs[$i]
+		PartitionKey = "NCclients"
+		RowKey       = $IDs[$i]
 	}
-	Add-AzDataTableEntity @Table -Entity $CustomerList -Force
-}         
-else {
-	$CustomerList = $Rows
 }
+Add-AzDataTableEntity @Table -Entity $CustomerList -Force
+
 
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
 		StatusCode = [HttpStatusCode]::OK
