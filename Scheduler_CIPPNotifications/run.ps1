288dbb8c390c28096cac0e449ac711405adfea12
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-01-23 04:55:26 -0700

    Update run.ps1

--- Scheduler_CIPPNotifications/run.ps1
+++ Scheduler_CIPPNotifications/run.ps1
@@ -195,7 +195,41 @@ else {
 
   if ($CurrentLog) {
     try {
-      if ($config.alerting -like 'onePerTenant') {
+      if ($config.alerting -like 'onePerAlert') {
+        if ($Config.email -like '*@*') {
+          foreach ($tenant in ($CurrentLog.Tenant | Sort-Object -Unique)) {
+            $TenantsAlerts = $CurrentLog | Select-Object Message, API, Tenant, Username, Severity | Where-Object -Property tenant -EQ $tenant
+            foreach ($Alert in $TenantsAlerts) {
+              $HTMLLog = ($Alert | ConvertTo-Html -frag) -replace '<table>', '<table class=blueTable>' | Out-String
+              $JSONBody = @"
+                    {
+                        "message": {
+                          "subject": "CIPP Alert: Alert $($Alert.Message) found for $($tenant) starting at $((Get-Date).AddMinutes(-15))",
+                          "body": {
+                            "contentType": "HTML",
+                            "content": "You've setup your alert policies to be alerted whenever specific events happen. We've found some of these events in the log:<br><br>
+      <style>table.blueTable{border:1px solid #1C6EA4;background-color:#EEE;width:100%;text-align:left;border-collapse:collapse}table.blueTable td,table.blueTable th{border:1px solid #AAA;padding:3px 2px}table.blueTable tbody td{font-size:13px}table.blueTable tr:nth-child(even){background:#D0E4F5}table.blueTable thead{background:#1C6EA4;background:-moz-linear-gradient(top,#5592bb 0,#327cad 66%,#1C6EA4 100%);background:-webkit-linear-gradient(top,#5592bb 0,#327cad 66%,#1C6EA4 100%);background:linear-gradient(to bottom,#5592bb 0,#327cad 66%,#1C6EA4 100%);border-bottom:2px solid #444}table.blueTable thead th{font-size:15px;font-weight:700;color:#FFF;border-left:2px solid #D0E4F5}table.blueTable thead th:first-child{border-left:none}table.blueTable tfoot{font-size:14px;font-weight:700;color:#FFF;background:#D0E4F5;background:-moz-linear-gradient(top,#dcebf7 0,#d4e6f6 66%,#D0E4F5 100%);background:-webkit-linear-gradient(top,#dcebf7 0,#d4e6f6 66%,#D0E4F5 100%);background:linear-gradient(to bottom,#dcebf7 0,#d4e6f6 66%,#D0E4F5 100%);border-top:2px solid #444}table.blueTable tfoot td{font-size:14px}table.blueTable tfoot .links{text-align:right}table.blueTable tfoot .links a{display:inline-block;background:#1C6EA4;color:#FFF;padding:2px 8px;border-radius:5px}</style>
+                            
+                            $($HTMLLog)
+                            
+                            "
+                          },
+                          "toRecipients": [
+                            {
+                              "emailAddress": {
+                                "address": "$($Config.email)"
+                              }
+                            }
+                          ]
+                        },
+                        "saveToSentItems": "false"
+                      }
+"@
+              New-GraphPostRequest -uri 'https://graph.microsoft.com/v1.0/me/sendMail' -tenantid $env:TenantID -type POST -body ($JSONBody)
+            }
+          }
+        }
+      }elseif ($config.alerting -like 'onePerTenant') {
         if ($Config.email -like '*@*') {
           foreach ($tenant in ($CurrentLog.Tenant | Sort-Object -Unique)) {
             $HTMLLog = ($CurrentLog | Select-Object Message, API, Tenant, Username, Severity | Where-Object -Property tenant -EQ $tenant | ConvertTo-Html -frag) -replace '<table>', '<table class=blueTable>' | Out-String
