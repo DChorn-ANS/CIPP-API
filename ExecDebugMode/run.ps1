bee01f1b6014079cadd30d7def34a541f6eac39d
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-01-27 10:32:22 -0700

    Update run.ps1

--- ExecDebugMode/run.ps1
+++ ExecDebugMode/run.ps1
@@ -12,7 +12,7 @@ if ($($request.body.setDebugMode)) {
     Try {
         Connect-AzAccount -Identity
         $Subscription = ($ENV:WEBSITE_OWNER_NAME).split('+') | Select-Object -First 1
-        Update-AZFunctionAppSetting -Name $($ENV:WEBSITE_SITE_NAME) -ResourceGroupName $($ENV:Website_Resource_Group) -SubscriptionId $Subscription -AppSetting @{"DebugMode" = "$($request.Query.setDebugMode)" } -Force
+        Update-AZFunctionAppSetting -Name $($ENV:WEBSITE_SITE_NAME) -ResourceGroupName $($ENV:Website_Resource_Group) -SubscriptionId $Subscription -AppSetting @{"DebugMode" = "$($request.body.setDebugMode)" } -Force
         $GraphRequest = [pscustomobject]@{'Results' = "Set Debug Mode to $($Request.body.setDebugMode)" }
         Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message "Set Debug Mode to $($request.Query.setDebugMode)" -Sev 'info'
     }
