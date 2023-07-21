763f3c8b2165185188db3b23d31d99875b1bc56b
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-07-21 11:22:10 -0600

    Update run.ps1

--- ListDefenderForOffice/run.ps1
+++ ListDefenderForOffice/run.ps1
@@ -11,7 +11,27 @@ $Function = $request.Query.function
 try {
     $Policies = New-ExoRequest -tenantid $Tenantfilter -cmdlet "Get-$($Function)Policy" | Select-Object * -ExcludeProperty *odata*, *data.type*
     $RuleState = New-ExoRequest -tenantid $Tenantfilter -cmdlet "Get-$($Function)Rule" | Select-Object * -ExcludeProperty *odata*, *data.type*
-    $GraphRequest = $Policies | Select-Object *, @{l = 'ruleState'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).State } }, @{l = 'rulePrio'; e = { $name = $_.name; (if ($name = "Standard Preset Security Policy") { -1 }elseif ($name = "Strict Preset Security Policy") { -2 }else { ($RuleState | Where-Object name -EQ $name).Priority }) } }
+    $GraphRequest = $Policies | Select-Object *, {
+        @{l = 'ruleState'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).State } },
+        @{l = 'rulePrio'; e = { $name = $_.name; {
+            (if ($name = "Standard Preset Security Policy") { -1 
+                    }elseif ($name = "Strict Preset Security Policy") { -2 
+                    }else { ($RuleState | Where-Object name -EQ $name).Priority }
+            ) } }
+        },
+        @{l = 'ruleInclUsers'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).IncludedSenders -join "<br />" } },
+        @{l = 'ruleInclUsersCount'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).IncludedSenders | Measure-Object | Select-Object -ExpandProperty Count } },
+        @{l = 'ruleInclGroups'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).IncludedGroups -join "<br />" } },
+        @{l = 'ruleInclGroupsCount'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).IncludedGroups  | Measure-Object | Select-Object -ExpandProperty Count } },
+        @{l = 'ruleInclDomains'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).IncludedDomains -join "<br />" } },
+        @{l = 'ruleInclDomainsCount'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).IncludedDomains  | Measure-Object | Select-Object -ExpandProperty Count } }, 
+        @{l = 'ruleExclUsers'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).ExcludedSenders -join "<br />" } },
+        @{l = 'ruleInclUsersCount'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).ExcludedSenders | Measure-Object | Select-Object -ExpandProperty Count } }, 
+        @{l = 'ruleExclGroups'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).ExcludedGroups -join "<br />" } },
+        @{l = 'ruleInclGroupCount'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).ExcludedGroups | Measure-Object | Select-Object -ExpandProperty Count } }, 
+        @{l = 'ruleExclDomains'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).ExcludedDomains -join "<br />" } },
+        @{l = 'ruleExclDomainsCount'; e = { $name = $_.name; ($RuleState | Where-Object name -EQ $name).ExcludedDomains | Measure-Object | Select-Object -ExpandProperty Count } }
+    }
     $StatusCode = [HttpStatusCode]::OK
 }
 catch {
